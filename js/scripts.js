c23699facfb05201a22dc1596fcec6e2cb4f5712
Turrelius <101061110+Turrelius@users.noreply.github.com>
2022-05-04 18:48:20 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+alert('Hello world');
+
+let favoriteFood = 'Pizza';
+document.write(favoriteFood);
+
+let favoriteWord = "flabergasted";
+document.write(favoriteWord);
+
+//let simpleAddition = 2 + 2;
+//document.write(simpleAddition);
+
+//let size = 100;
+//let doubleSize = size * 2;
+//document.write(doubleSize);
+
+let size = 150;
+let doubleSize = size * 3;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
