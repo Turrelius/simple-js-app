3236380e54c0a1ec4189a56cf332acd723ec2bdf
Turrelius <101061110+Turrelius@users.noreply.github.com>
2022-05-24 18:18:34 +0200

    Update scripts.js
    added arrays and and added = to declear variable

--- js/scripts.js
+++ js/scripts.js
@@ -12,29 +12,29 @@ alert('Hello world');
     // document.write('My name is ' + name + ' and I\'m ' + age + ' years old!');
     //console.log('My name is ' + name + ' and I\'m ' + age + ' years old!');
 
-let pokemonList {
-
-    name: "Sandshrew",
-    height: 6,
-    types: ["ground"],
-    category: "mouse"
-  },
-  {
-    name: "Kabutops",
-    height: 13,
-    types: ["water", "rock"],
-    category: "Shell Pokémon"
-  },
-  {
-      name: "Scyther",
-    height: 15,
-    types: ["bug","flying"],
-    category: "Praying Mantis Pokémon"
-  },
-  {
-    name: "Zoroark"
-    height: 2,
-    types: ["dark"],
-    category: "Illusory Fox Pokémon"
-  },
-  ];
+    let pokemonList = [
+      {
+        name: "Sandshrew",
+        height: 6,
+        types: ["ground"],
+        category: "mouse",
+      },
+      {
+        name: "Kabutops",
+        height: 13,
+        types: ["water", "rock"],
+        category: "Shell Pokémon",
+      },
+      {
+        name: "Scyther",
+        height: 15,
+        types: ["bug", "flying"],
+        category: "Praying Mantis Pokémon",
+      },
+      {
+        name: "Zoroark",
+        height: 2,
+        types: ["dark"],
+        category: "Illusory Fox Pokémon",
+      },
+    ];
