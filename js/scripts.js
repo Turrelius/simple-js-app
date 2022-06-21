9e4f3e6e4e2055d262a34d3d6badec1bf3a67494
Turrelius <101061110+Turrelius@users.noreply.github.com>
2022-06-21 19:10:30 +0200

    loops
    trying to ad loops to my script

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,14 @@ alert('Hello world');
     // document.write('My name is ' + name + ' and I\'m ' + age + ' years old!');
     //console.log('My name is ' + name + ' and I\'m ' + age + ' years old!');
 
-    let pokemonList = [
+let pokemonList = [
+// pokemon objects
+];
+//simple loop that iterates over each item in pokemonList.
+//I used document.write() inside the loop’s code to write the Pokémon name on my website’s DOM
+for (let i = 0; i < pokemonList.length; i++){
+  document.write(pokemonList[i].name + " ");
+}
       {
         name: "Sandshrew",
         height: 6,
