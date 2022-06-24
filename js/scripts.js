c6ab567382ae0319a8e28e004eb65f0aa7636a57
Turrelius <101061110+Turrelius@users.noreply.github.com>
2022-06-24 18:14:18 +0200

    Update scripts.js
    reorganized and fixed array issue

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,7 @@
 alert ('Hello pokemon fans!!');
 
-//variable called "pokemonList" that is an array of objects
+//A variable called "pokemonList" that is an array of objects
 let pokemonList = [
-];
-//simple loop that iterates over each item in pokemonList.
-//I used document.write() inside the loop’s code to write the Pokémon name on my website’s DOM
-for (let i = 0; i < pokemonList.length; i++){
-   // First print the pokemon name and height
-   document.write(
-     pokemonList[i].name + " (Height: " + pokemonList[i].height + ")"
-   );
- // run statement that checks if the pokemons height is over 14
-   if (pokemonList[i].height > 14) {
-     // If it is over 14 then print " - Wow, that's big !" with two line breaks to separate the pokemon
-     document.write(" - Wow, that's big !" + "</br>" + "</br>");
-   } else
-   if (pokemonList[i].height < 14) {
-     // If it is less than 14 then print " - Oboy,thats small !" with two line breaks to separate the pokemon
-     document.write(" - Oboy, that's small !" + "</br>" + "</br>");
-   } else {
-     // If the pokemon height is not over 14 then print two line breaks to separate the pokemon
-     document.write("</br>" + "</br>");
-   }
- }
       {
         name: "Sandshrew",
         height: 6,
@@ -48,3 +27,18 @@ for (let i = 0; i < pokemonList.length; i++){
         category: "Illusory Fox Pokémon",
       },
     ];
+// Loop through each pokemon (AKA, for every pokemon in the array, run some code)
+for (let i = 0; i < pokemonList.length; i++) {
+  // First print the pokemon name and height
+  document.write(
+    pokemonList[i].name + " (Height: " + pokemonList[i].height + ")"
+  );
+// Then run an if statement that checks if the pokemons height is over 14
+  if (pokemonList[i].height > 14) {
+    // If it is over 14 then print " - Wow, that's big !" with two line breaks to separate the pokemon
+    document.write(" - Wow, that's big !" + "</br>" + "</br>");
+  } else {
+    // If the pokemon height is not over 14 then print two line breaks to separate the pokemon
+    document.write("</br>" + "</br>");
+  }
+}
